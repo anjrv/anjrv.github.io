d2eec416bc0ce2685260c567525fe61a4c57234a
Anna Jaerving <AnnaJrving@gmail.com>
2022-01-18 17:00:40 +0000

    hw2 progress

---
+++ hw2/3/circlefan_files/circlefan.js
@@ -0,0 +1,81 @@
+/////////////////////////////////////////////////////////////////
+//    S�nid�mi � T�lvugraf�k
+//     Teikna n�lgun � hring sem TRIANGLE_FAN
+//
+//    Hj�lmt�r Hafsteinsson, jan�ar 2022
+/////////////////////////////////////////////////////////////////
+var canvas;
+var gl;
+
+// numCirclePoints er fj�ldi punkta � hringnum
+// Heildarfj�ldi punkta er tveimur meiri (mi�punktur + fyrsti punktur kemur tvisvar)
+var numCirclePoints = 4;
+
+var radius = 0.4;
+var center = vec2(0, 0);
+
+var points = [];
+
+function init() {
+  canvas = document.getElementById('gl-canvas');
+
+  gl = WebGLUtils.setupWebGL(canvas);
+  if (!gl) {
+    alert("WebGL isn't available");
+  }
+
+  gl.viewport(0, 0, canvas.width, canvas.height);
+  gl.clearColor(1.0, 1.0, 1.0, 1.0);
+
+  //
+  //  Load shaders and initialize attribute buffers
+  //
+  var program = initShaders(gl, 'vertex-shader', 'fragment-shader');
+  gl.useProgram(program);
+
+  // Create the circle
+  createCirclePoints(center, radius, numCirclePoints);
+
+  var vBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, 8*Math.pow(3, 7), gl.STATIC_DRAW);
+
+  var vPosition = gl.getAttribLocation(program, 'vPosition');
+  gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(vPosition);
+
+  document.getElementById('slider').onchange = function (event) {
+    numCirclePoints = event.target.value;
+
+    render();
+  };
+
+  render();
+};
+
+// Create the points of the circle
+function createCirclePoints(cent, rad, k) {
+  points = [];
+
+  points.push(cent);
+
+  var dAngle = (2 * Math.PI) / k;
+  for (i = k; i >= 0; i--) {
+    a = i * dAngle;
+    var p = vec2(rad * Math.sin(a) + cent[0], rad * Math.cos(a) + cent[1]);
+    points.push(p);
+  }
+}
+
+window.onload = init;
+
+function render() {
+  createCirclePoints(center, radius, numCirclePoints);
+
+  gl.bufferData(gl.ARRAY_BUFFER, 8*Math.pow(3, 7), gl.STATIC_DRAW);
+  gl.bufferSubData(gl.ARRAY_BUFFER, 0, flatten(points));
+  gl.clear(gl.COLOR_BUFFER_BIT);
+  gl.drawArrays(gl.TRIANGLE_FAN, 0, numCirclePoints + 2);
+
+  // window.requestAnimFrame(render);
+}
---
+++ hw2/5/points_files/points.js
@@ -0,0 +1,73 @@
+/////////////////////////////////////////////////////////////////
+//    S�nid�mi � T�lvugraf�k
+//     Teiknar punkt � strigann �ar sem notandinn smellir m�sinni
+//
+//    Hj�lmt�r Hafsteinsson, jan�ar 2021
+/////////////////////////////////////////////////////////////////
+var canvas;
+var gl;
+
+var maxNumTriangles = 200; // H�marksfj�ldi punkta sem forriti� r��ur vi�!
+var index = 0; // N�mer n�verandi punkts
+
+window.onload = function init() {
+  canvas = document.getElementById('gl-canvas');
+
+  gl = WebGLUtils.setupWebGL(canvas);
+  if (!gl) {
+    alert("WebGL isn't available");
+  }
+
+  gl.viewport(0, 0, canvas.width, canvas.height);
+  gl.clearColor(0.0, 0.0, 1.0, 1.0);
+
+  //
+  //  Load shaders and initialize attribute buffers
+  //
+  var program = initShaders(gl, 'vertex-shader', 'fragment-shader');
+  gl.useProgram(program);
+
+  var vBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, 8 * 3 * maxNumTriangles, gl.DYNAMIC_DRAW);
+
+  var vPosition = gl.getAttribLocation(program, 'vPosition');
+  gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(vPosition);
+
+  canvas.addEventListener('mousedown', function (e) {
+    if (index === maxNumTriangles) return;
+
+    if (e.button === 2) {
+      gl.bufferData(gl.ARRAY_BUFFER, 24 * maxNumTriangles, gl.DYNAMIC_DRAW);
+      index = 0;
+
+      return;
+    }
+
+    const x = (2 * e.offsetX) / canvas.width - 1;
+    const y = (2 * (canvas.height - e.offsetY)) / canvas.height - 1;
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+    gl.bufferSubData(
+      gl.ARRAY_BUFFER,
+      24 * index,
+      flatten([
+        vec2(x - 0.05, y - 0.05),
+        vec2(x, y + 0.05),
+        vec2(x + 0.05, y - 0.05),
+      ]),
+    );
+
+    index++;
+  });
+
+  render();
+};
+
+function render() {
+  gl.clear(gl.COLOR_BUFFER_BIT);
+  gl.drawArrays(gl.TRIANGLES, 0, index);
+
+  window.requestAnimFrame(render);
+}
