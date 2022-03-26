2c592f69b8b6872d2058ec0ac0db341696a44ed7
Anna Jaerving <AnnaJrving@gmail.com>
2022-03-26 14:03:11 +0000

    hw7 q2

--- hw7/VeggurGolf_files/VeggurGolf.js
+++ hw7/VeggurGolf_files/VeggurGolf.js
@@ -1,15 +1,7 @@
-/////////////////////////////////////////////////////////////////
-//    Sýnidæmi í Tölvugrafík
-//     Forrit með tveimur mynstrum.  Sýnir vegg með
-//     múrsteinsmynstri og gólf með viðarmynstri.  Það er hægt
-//     að ganga um líkanið, en það er engin árekstarvörn.
-//
-//    Hjálmtýr Hafsteinsson, mars 2022
-/////////////////////////////////////////////////////////////////
 var canvas;
 var gl;
 
-var numVertices  = 6;
+var numVertices = 9;
 
 var program;
 
@@ -23,12 +15,11 @@ var texGolf;
 
 // Breytur fyrir hreyfingu áhorfanda
 var userXPos = 0.0;
-var userZPos = 2.0;
-var userIncr = 0.1;                // Size of forward/backward step
-var userAngle = 270.0;             // Direction of the user in degrees
-var userXDir = 0.0;                // X-coordinate of heading
-var userZDir = -1.0;               // Z-coordinate of heading
-
+var userZPos = 0.0;
+var userIncr = 0.1; // Size of forward/backward step
+var userAngle = 180.0; // Direction of the user in degrees
+var userXDir = -1; // X-coordinate of heading
+var userZDir = 0.0; // Z-coordinate of heading
 
 var movement = false;
 var spinX = 0;
@@ -43,176 +34,217 @@ var mvLoc;
 
 // Hnútar veggsins
 var vertices = [
-    vec4( -5.0,  0.0, 0.0, 1.0 ),
-    vec4(  5.0,  0.0, 0.0, 1.0 ),
-    vec4(  5.0,  1.0, 0.0, 1.0 ),
-    vec4(  5.0,  1.0, 0.0, 1.0 ),
-    vec4( -5.0,  1.0, 0.0, 1.0 ),
-    vec4( -5.0,  0.0, 0.0, 1.0 ),
-// Hnútar gólfsins (strax á eftir)
-    vec4( -5.0,  0.0, 10.0, 1.0 ),
-    vec4(  5.0,  0.0, 10.0, 1.0 ),
-    vec4(  5.0,  0.0,  0.0, 1.0 ),
-    vec4(  5.0,  0.0,  0.0, 1.0 ),
-    vec4( -5.0,  0.0,  0.0, 1.0 ),
-    vec4( -5.0,  0.0, 10.0, 1.0 )
+  vec4(-10.0, 0.0, -1.0, 1.0),
+  vec4(10.0, 0.0, -1.0, 1.0),
+  vec4(10.0, 1.0, -1.0, 1.0),
+  vec4(10.0, 1.0, -1.0, 1.0),
+  vec4(-10.0, 1.0, -1.0, 1.0),
+  vec4(-10.0, 0.0, -1.0, 1.0),
+
+  vec4(-10.0, 0.0, 1.0, 1.0),
+  vec4(10.0, 0.0, 1.0, 1.0),
+  vec4(10.0, 1.0, 1.0, 1.0),
+  vec4(10.0, 1.0, 1.0, 1.0),
+  vec4(-10.0, 1.0, 1.0, 1.0),
+  vec4(-10.0, 0.0, 1.0, 1.0),
+
+  //// Hnútar gólfsins (strax á eftir)
+  vec4(-10.0, 0.0, 1.0, 1.0),
+  vec4(10.0, 0.0, 1.0, 1.0),
+  vec4(10.0, 0.0, -1.0, 1.0),
+  vec4(10.0, 0.0, -1.0, 1.0),
+  vec4(-10.0, 0.0, -1.0, 1.0),
+  vec4(-10.0, 0.0, 1.0, 1.0),
 ];
 
 // Mynsturhnit fyrir vegg
 var texCoords = [
-    vec2(  0.0, 0.0 ),
-    vec2( 10.0, 0.0 ),
-    vec2( 10.0, 1.0 ),
-    vec2( 10.0, 1.0 ),
-    vec2(  0.0, 1.0 ),
-    vec2(  0.0, 0.0 ),
-// Mynsturhnit fyrir gólf
-    vec2(  0.0,  0.0 ),
-    vec2( 10.0,  0.0 ),
-    vec2( 10.0, 10.0 ),
-    vec2( 10.0, 10.0 ),
-    vec2(  0.0, 10.0 ),
-    vec2(  0.0,  0.0 )
+  vec2(0.0, 0.0),
+  vec2(20.0, 0.0),
+  vec2(20.0, 1.0),
+  vec2(20.0, 1.0),
+  vec2(0.0, 1.0),
+  vec2(0.0, 0.0),
+
+  vec2(0.0, 0.0),
+  vec2(20.0, 0.0),
+  vec2(20.0, 1.0),
+  vec2(20.0, 1.0),
+  vec2(0.0, 1.0),
+  vec2(0.0, 0.0),
+
+  // Mynsturhnit fyrir gólf
+  vec2(-10.0, -1.0),
+  vec2(10.0, -1.0),
+  vec2(10.0, 1.0),
+  vec2(10.0, 1.0),
+  vec2(-10.0, 1.0),
+  vec2(-10.0, -1.0),
 ];
 
-
 window.onload = function init() {
+  canvas = document.getElementById('gl-canvas');
 
-    canvas = document.getElementById( "gl-canvas" );
-    
-    gl = WebGLUtils.setupWebGL( canvas );
-    if ( !gl ) { alert( "WebGL isn't available" ); }
+  gl = WebGLUtils.setupWebGL(canvas);
+  if (!gl) {
+    alert("WebGL isn't available");
+  }
 
-    gl.viewport( 0, 0, canvas.width, canvas.height );
-    gl.clearColor( 0.9, 1.0, 1.0, 1.0 );
-    
-    gl.enable(gl.DEPTH_TEST);
+  gl.viewport(0, 0, canvas.width, canvas.height);
+  gl.clearColor(0.9, 1.0, 1.0, 1.0);
 
-    //
-    //  Load shaders and initialize attribute buffers
-    //
-    program = initShaders( gl, "vertex-shader", "fragment-shader" );
-    gl.useProgram( program );
-    
-    var vBuffer = gl.createBuffer();
-    gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer );
-    gl.bufferData( gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW );
-    
-    var vPosition = gl.getAttribLocation( program, "vPosition" );
-    gl.vertexAttribPointer( vPosition, 4, gl.FLOAT, false, 0, 0 );
-    gl.enableVertexAttribArray( vPosition );
-    
-    var tBuffer = gl.createBuffer();
-    gl.bindBuffer( gl.ARRAY_BUFFER, tBuffer );
-    gl.bufferData( gl.ARRAY_BUFFER, flatten(texCoords), gl.STATIC_DRAW );
-    
-    var vTexCoord = gl.getAttribLocation( program, "vTexCoord" );
-    gl.vertexAttribPointer( vTexCoord, 2, gl.FLOAT, false, 0, 0 );
-    gl.enableVertexAttribArray( vTexCoord );
+  gl.enable(gl.DEPTH_TEST);
 
-    // Lesa inn og skilgreina mynstur fyrir vegg
-    var veggImage = document.getElementById("VeggImage");
-    texVegg = gl.createTexture();
-    gl.bindTexture( gl.TEXTURE_2D, texVegg );
-    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
-    gl.texImage2D( gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, veggImage );
-    gl.generateMipmap( gl.TEXTURE_2D );
-    gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_LINEAR );
-    gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR );
-    
-    gl.uniform1i(gl.getUniformLocation(program, "texture"), 0);
+  //
+  //  Load shaders and initialize attribute buffers
+  //
+  program = initShaders(gl, 'vertex-shader', 'fragment-shader');
+  gl.useProgram(program);
 
-    // Lesa inn og skilgreina mynstur fyrir gólf
-    var golfImage = document.getElementById("GolfImage");
-    texGolf = gl.createTexture();
-    gl.bindTexture( gl.TEXTURE_2D, texGolf );
-    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
-    gl.texImage2D( gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, golfImage );
-    gl.generateMipmap( gl.TEXTURE_2D );
-    gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_LINEAR );
-    gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR );
-    
-    gl.uniform1i(gl.getUniformLocation(program, "texture"), 0);
+  var vBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW);
 
+  var vPosition = gl.getAttribLocation(program, 'vPosition');
+  gl.vertexAttribPointer(vPosition, 4, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(vPosition);
 
-    proLoc = gl.getUniformLocation( program, "projection" );
-    mvLoc = gl.getUniformLocation( program, "modelview" );
+  var tBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, tBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, flatten(texCoords), gl.STATIC_DRAW);
 
-    var proj = perspective( 50.0, 1.0, 0.2, 100.0 );
-    gl.uniformMatrix4fv(proLoc, false, flatten(proj));
-    
+  var vTexCoord = gl.getAttribLocation(program, 'vTexCoord');
+  gl.vertexAttribPointer(vTexCoord, 2, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(vTexCoord);
 
-    //event listeners for mouse
-    canvas.addEventListener("mousedown", function(e){
-        movement = true;
-        origX = e.clientX;
-    } );
+  // Lesa inn og skilgreina mynstur fyrir vegg
+  var veggImage = document.getElementById('VeggImage');
+  texVegg = gl.createTexture();
+  gl.bindTexture(gl.TEXTURE_2D, texVegg);
+  gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+  gl.texImage2D(
+    gl.TEXTURE_2D,
+    0,
+    gl.RGBA,
+    gl.RGBA,
+    gl.UNSIGNED_BYTE,
+    veggImage,
+  );
+  gl.generateMipmap(gl.TEXTURE_2D);
+  gl.texParameteri(
+    gl.TEXTURE_2D,
+    gl.TEXTURE_MIN_FILTER,
+    gl.LINEAR_MIPMAP_LINEAR,
+  );
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
 
-    canvas.addEventListener("mouseup", function(e){
-        movement = false;
-    } );
+  gl.uniform1i(gl.getUniformLocation(program, 'texture'), 0);
 
-    canvas.addEventListener("mousemove", function(e){
-        if(movement) {
-            userAngle += 0.4*(origX - e.clientX);
-            userAngle %= 360.0;
-            userXDir = Math.cos( radians(userAngle) );
-            userZDir = Math.sin( radians(userAngle) );
-            origX = e.clientX;
-        }
-    } );
-    
-    // Event listener for keyboard
-     window.addEventListener("keydown", function(e){
-         switch( e.keyCode ) {
-            case 87:	// w
-                userXPos += userIncr * userXDir;
-                userZPos += userIncr * userZDir;;
-                break;
-            case 83:	// s
-                userXPos -= userIncr * userXDir;
-                userZPos -= userIncr * userZDir;;
-                break;
-            case 65:	// a
-                userXPos += userIncr * userZDir;
-                userZPos -= userIncr * userXDir;;
-                break;
-            case 68:	// d
-                userXPos -= userIncr * userZDir;
-                userZPos += userIncr * userXDir;;
-                break;
-         }
-     }  );  
+  // Lesa inn og skilgreina mynstur fyrir gólf
+  var golfImage = document.getElementById('GolfImage');
+  texGolf = gl.createTexture();
+  gl.bindTexture(gl.TEXTURE_2D, texGolf);
+  gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+  gl.texImage2D(
+    gl.TEXTURE_2D,
+    0,
+    gl.RGBA,
+    gl.RGBA,
+    gl.UNSIGNED_BYTE,
+    golfImage,
+  );
+  gl.generateMipmap(gl.TEXTURE_2D);
+  gl.texParameteri(
+    gl.TEXTURE_2D,
+    gl.TEXTURE_MIN_FILTER,
+    gl.LINEAR_MIPMAP_LINEAR,
+  );
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
 
-    // Event listener for mousewheel
-     window.addEventListener("wheel", function(e){
-         if( e.deltaY > 0.0 ) {
-             zDist += 0.2;
-         } else {
-             zDist -= 0.2;
-         }
-     }  );  
+  gl.uniform1i(gl.getUniformLocation(program, 'texture'), 0);
 
+  proLoc = gl.getUniformLocation(program, 'projection');
+  mvLoc = gl.getUniformLocation(program, 'modelview');
 
-    render();
- 
-}
+  var proj = perspective(50.0, 1.0, 0.2, 100.0);
+  gl.uniformMatrix4fv(proLoc, false, flatten(proj));
 
-var render = function(){
-    gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+  //event listeners for mouse
+  canvas.addEventListener('mousedown', function (e) {
+    movement = true;
+    origX = e.clientX;
+  });
 
-    // staðsetja áhorfanda og meðhöndla músarhreyfingu
-    var mv = lookAt( vec3(userXPos, 0.5, userZPos), vec3(userXPos+userXDir, 0.5, userZPos+userZDir), vec3(0.0, 1.0, 0.0 ) );
-    
-    gl.uniformMatrix4fv(mvLoc, false, flatten(mv));
+  canvas.addEventListener('mouseup', function (e) {
+    movement = false;
+  });
 
-    // Teikna vegg með mynstri
-    gl.bindTexture( gl.TEXTURE_2D, texVegg );
-    gl.drawArrays( gl.TRIANGLES, 0, numVertices );
+  canvas.addEventListener('mousemove', function (e) {
+    if (movement) {
+      userAngle += 0.4 * (origX - e.clientX);
+      userAngle %= 360.0;
+      userXDir = Math.cos(radians(userAngle));
+      userZDir = Math.sin(radians(userAngle));
+      origX = e.clientX;
+    }
+  });
 
-    // Teikna gólf með mynstri
-    gl.bindTexture( gl.TEXTURE_2D, texGolf );
-    gl.drawArrays( gl.TRIANGLES, numVertices, numVertices );
+  // Event listener for keyboard
+  window.addEventListener('keydown', function (e) {
+    switch (e.keyCode) {
+      case 87: // w
+        userXPos += userIncr * userXDir;
+        userZPos += userIncr * userZDir;
+        break;
+      case 83: // s
+        userXPos -= userIncr * userXDir;
+        userZPos -= userIncr * userZDir;
+        break;
+      case 65: // a
+        userXPos += userIncr * userZDir;
+        userZPos -= userIncr * userXDir;
+        break;
+      case 68: // d
+        userXPos -= userIncr * userZDir;
+        userZPos += userIncr * userXDir;
+        break;
+    }
 
-    requestAnimFrame(render);
-}
+    // Clamp Z to walls
+    userZPos = userZPos > 0.75 ? 0.75 : userZPos < -0.75 ? -0.75 : userZPos;
+  });
+
+  // Event listener for mousewheel
+  window.addEventListener('wheel', function (e) {
+    if (e.deltaY > 0.0) {
+      zDist += 0.2;
+    } else {
+      zDist -= 0.2;
+    }
+  });
+
+  render();
+};
+
+var render = function () {
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+  // staðsetja áhorfanda og meðhöndla músarhreyfingu
+  var mv = lookAt(
+    vec3(userXPos, 0.5, userZPos),
+    vec3(userXPos + userXDir, 0.5, userZPos + userZDir),
+    vec3(0.0, 1.0, 0.0),
+  );
+
+  gl.uniformMatrix4fv(mvLoc, false, flatten(mv));
+
+  // Teikna vegg með mynstri
+  gl.bindTexture(gl.TEXTURE_2D, texVegg);
+  gl.drawArrays(gl.TRIANGLES, 0, numVertices + 3);
+
+  // Teikna gólf með mynstri
+  gl.bindTexture(gl.TEXTURE_2D, texGolf);
+  gl.drawArrays(gl.TRIANGLES, numVertices, numVertices);
+
+  requestAnimFrame(render);
+};
