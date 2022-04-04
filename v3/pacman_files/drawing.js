2198afa1bff4f46af8207d706dd9aac40bf878be
Anna Jaerving <AnnaJrving@gmail.com>
2022-04-04 15:57:16 +0000

    eyes.. sort of

--- v3/pacman_files/drawing.js
+++ v3/pacman_files/drawing.js
@@ -46,10 +46,39 @@ function createPowerUp() {
 }
 
 function createPacman() {
-  const geom = new THREE.SphereGeometry(PACMAN_RADIUS, 32, 32, 0, Math.PI * 1.8);
+  const geom = new THREE.SphereGeometry(
+    PACMAN_RADIUS,
+    32,
+    32,
+    0,
+    Math.PI * 1.8,
+  );
   const mat = new THREE.MeshStandardMaterial({ color: '#FFFF31' });
 
   const pacman = new THREE.Mesh(geom, mat);
+
+  const eye1 = new THREE.Mesh(
+    new THREE.SphereGeometry(DOT_RADIUS),
+    new THREE.MeshLambertMaterial({ color: '#000000' }),
+  );
+
+  eye1.position.x -= PACMAN_RADIUS * 0.7;
+  eye1.position.y += PACMAN_RADIUS * 0.3;
+  eye1.position.z += PACMAN_RADIUS * 0.5;
+
+  pacman.add(eye1);
+
+  const eye2 = new THREE.Mesh(
+    new THREE.SphereGeometry(DOT_RADIUS),
+    new THREE.MeshLambertMaterial({ color: '#000000' }),
+  );
+
+  eye2.position.x -= PACMAN_RADIUS * 0.7;
+  eye2.position.y -= PACMAN_RADIUS * 0.3;
+  eye2.position.z += PACMAN_RADIUS * 0.5;
+
+  pacman.add(eye2);
+
   pacman.direction = new THREE.Vector3(-1, 0, 0);
   pacman.isPacman = true;
   pacman.isMoving = false;
